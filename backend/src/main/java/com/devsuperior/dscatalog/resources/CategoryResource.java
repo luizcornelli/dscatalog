b47c44504a1037f0b8006d47f3c8ac39500bbdb6
Luiz Cornelli <luizcornelli.dev@gmail.com>
2021-01-14 16:46:42 -0300

    Delete category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,5 +60,13 @@ public class CategoryResource {
 		categoryDTO = categoryService.update(id, categoryDTO);
 		
 		return ResponseEntity.ok().body(categoryDTO);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> update(@PathVariable Long id){
+		
+		categoryService.delete(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -18,14 +19,33 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException entityNotFoundException,
 			HttpServletRequest httpServletRequest) {
 		
+		HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+		
 		StandardError standardError = new StandardError();
 		
 		standardError.setTimestamp(Instant.now());
-		standardError.setStatus(HttpStatus.NOT_FOUND.value());
+		standardError.setStatus(httpStatus.value());
 		standardError.setError("Resource not found");
 		standardError.setMessage(entityNotFoundException.getMessage());
 		standardError.setPath(httpServletRequest.getRequestURI());
 		
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
+		return ResponseEntity.status(httpStatus.value()).body(standardError);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> entityNotFound(DatabaseException databaseException,
+			HttpServletRequest httpServletRequest) {
+		
+		HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+		
+		StandardError standardError = new StandardError();
+		
+		standardError.setTimestamp(Instant.now());
+		standardError.setStatus(httpStatus.value());
+		standardError.setError("Database exception");
+		standardError.setMessage(databaseException.getMessage());
+		standardError.setPath(httpServletRequest.getRequestURI());
+		
+		return ResponseEntity.status(httpStatus.value()).body(standardError);
 	}
 }
