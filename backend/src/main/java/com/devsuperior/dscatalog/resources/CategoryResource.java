a8f902603b4aec64435fd43df88b0d521a13474f
Luiz Cornelli <luizcornelli.dev@gmail.com>
2021-01-14 11:53:34 -0300

    Find Category by id, Exception handler

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
-import com.devsuperior.dscatalog.service.CategoryService;
+import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -24,5 +25,13 @@ public class CategoryResource {
 		List<CategoryDTO> list = categoryService.findAll();
 		
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		
+		CategoryDTO dto = categoryService.findById(id);
+		
+		return ResponseEntity.ok().body(dto);
 	}
 }
---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dscatalog.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException entityNotFoundException,
+			HttpServletRequest httpServletRequest) {
+		
+		StandardError standardError = new StandardError();
+		
+		standardError.setTimestamp(Instant.now());
+		standardError.setStatus(HttpStatus.NOT_FOUND.value());
+		standardError.setError("Resource not found");
+		standardError.setMessage(entityNotFoundException.getMessage());
+		standardError.setPath(httpServletRequest.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
+	}
+}
